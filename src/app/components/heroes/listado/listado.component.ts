28471f961456670d41446cc056a00241b80e1a4c
Ander Arriaga <anderarriaga@gmail.com>
2021-09-23 22:01:39 +0200

    Primer commit

---
+++ src/app/components/heroes/listado/listado.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-listado',
+  templateUrl: './listado.component.html',
+})
+export class ListadoComponent {
+
+  heroeBorrado: string = "";
+  heroes: string[] = ["Spiderman", "Ironman", "Hulk", "Thor", "Capitan América"];
+  
+
+  borrarHeroe(){
+    this.heroeBorrado = this.heroes.pop() || "";
+  };
+}
